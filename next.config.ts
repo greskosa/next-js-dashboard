6effc617eb8a89956beb0d863b5943bed107a4b0
greskosa <greskosa@github.com>
2025-03-17 20:48:55 +0100

    add streeming for page and cpmponents

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  // experimental: {
+  //   ppr: 'incremental'
+  // }
 };
 
 export default nextConfig;
